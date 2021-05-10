a6b3f884eda19a6a2b22d5269caefe464dba23d6
sures <sures@LAPTOP-4M6LAJK0>
2021-05-10 11:08:38 -0400

    my test local

---
+++ src/SampleLocalRepo/TestLocalClass.java
@@ -0,0 +1,11 @@
+package SampleLocalRepo;
+
+public class TestLocalClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("new repository test");
+
+	}
+
+}
