4d22ac1275d6e7bdbbf20cc48b213458bb3de915
sures <sures@LAPTOP-4M6LAJK0>
2021-05-10 11:10:40 -0400

    second programs

---
+++ src/SampleLocalRepo/ExecutefromLocal.java
@@ -0,0 +1,11 @@
+package SampleLocalRepo;
+
+public class ExecutefromLocal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test the local to remote");
+
+	}
+
+}
